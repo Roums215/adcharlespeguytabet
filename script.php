1dbc39e4e9ab462cc1d786ec481bf89f454cecbe
Nicolas Peynet <nicolasp13009@gmail.com>
2024-12-05 16:14:01 +0100

    script inscription v2

--- script.php
+++ script.php
@@ -1,24 +1,31 @@
 <?php
-//INFOS BASE DE DONNEE
+// INFOS BASE DE DONNEE
 $servername = "mysql-hamdaouirayan.alwaysdata.net";
 $username = "351256";
 $password = "Rayan13k14";
 $dbname = "hamdaouirayan_tabet";
 
-//INFOS UTILISATEURS
+// INFOS UTILISATEURS
 $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 $email = $nom . "." . $prenom . "@charlespeguy.org";
 $mot_de_passe = $_POST["motdepasse"];
 $organizational_unit = $_POST["typeUser"];
 
-//CONNECTION A LA BDD
+// CONNECTION A LA BDD
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "INSERT INTO users (first_name, last_name, email,password,organizational_unit)
-VALUES ($nom,$prenom,$email,$mot_de_passe,$organizational_unit)";
+if (isset($_POST['validerInscrire'])) {
+    $sql = "INSERT INTO users (first_name, last_name, email, password, organizational_unit) 
+    VALUES ('$nom', '$prenom', '$email', '$mot_de_passe', '$organizational_unit')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New user created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
 ?>
 
